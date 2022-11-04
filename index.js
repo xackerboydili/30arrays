e0c882cba1f707aacdf6155f1b50ee11c5b3131d
xackerboydili <isoqovdilshod47@gmail.com>
2022-11-04 16:46:05 +0500

    Operators

---
+++ index.js
@@ -0,0 +1,131 @@
+
+/*let students_a_class = 23
+    students_b_class = 30
+    students_c_class = 29
+
+let students_of_school = students_a_class + students_b_class + students_c_class
+
+    console.log(students_of_school)*/
+
+
+
+
+
+/*let num1 = 32
+    num2 = 3
+    num3 = 5
+    num4 = 88
+
+let result = num1 * num2 / num3 + num4
+
+console.log(result)*/
+
+
+
+
+
+
+
+/*let money = {
+    valueOf(){
+       return 200 
+    }
+}
+
+let currentMoney = money +50
+
+console.log(currentMoney)*/
+
+/*let passangers = {
+    valueOf(){
+        return 50
+    }
+}
+let currentPassangers = passangers - 3
+
+console.log(currentPassangers)*/
+
+
+
+
+
+/*let remainder = 9 % -5;
+console.log(remainder); 
+
+remainder = 11 % 4;
+console.log(remainder); */
+
+/*let remainder = Infinity % 88
+console.log(remainder)*/
+
+
+
+
+
+
+/*let apple = 1000
+
+apple += 500
+
+console.log(apple)*/
+
+/*let students = 35
+
+students += 5
+
+console.log(students)*/
+
+
+
+/*let student = {
+    name: 'Dilshod',
+    valueOf () {
+      return '50';
+    },
+  };
+  
+  console.log(+student);*/
+
+  /*let a = 40, 
+      b = 80; 
+
+console.log(a <= b);  
+console.log(a != 40);*/
+
+/*let num1 = true
+    num2 = true
+
+console.log(num1 && num2)*/
+  
+
+// Arifmetik operatorlar
+// mantiqiy operatorlar
+
+// ||, &&, !, ??,  
+
+// Comparison operators
+
+// let a = '10';
+// let b = '10';
+// =, ==, ===,
+// let a=10;
+// let b='10';
+// b=b*1;
+// console.log(typeof b)
+
+// +b;
+// let c = a + +b;
+
+// console.log(c)
+
+// console.log(c)
+
+
+/*let a=10;
+let b= '10';
+b*=1*/
+
+
+let a=5;
+a++;
+console.log(++a)
